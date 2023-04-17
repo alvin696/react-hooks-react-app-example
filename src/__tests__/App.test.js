c93eba587a0821044492d8f4f61f38708bd40232
Alvin kipkirui yaem <alvinyaem4@gmail.com>
2023-04-17 16:10:47 +0300

    Completed assignment

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -15,23 +15,9 @@ test('should include "Now" in the header instead of a time', () => {
 });
 
 test("should include the <ExampleComponent />", () => {
-  expect(screen.queryByText("Whoa!")).toBeInTheDocument();
+  expect(screen.getByText("Whoa!")).toBeInTheDocument();
 });
 
 test("should include the <TestComponent />", () => {
-  expect(screen.queryByTitle("time video")).toBeInTheDocument();
+  expect(screen.getByTitle("time video")).toBeInTheDocument();
 });
-
-//   it('should include "Now" in the header instead of a time', () => {
-//     expect(wrapper.find('header').text()).to.not.include(moment().format('MMMM Do YYYY'))
-//     expect(wrapper.find('header').text()).to.include('Now')
-//   });
-
-//   it('should include the ExampleComponent', () => {
-//     expect(wrapper.text()).to.include('<ExampleComponent />')
-//   });
-
-//   it('should include the TestComponent', () => {
-//     expect(wrapper.text()).to.include('<TestComponent />')
-//   });
-// });
